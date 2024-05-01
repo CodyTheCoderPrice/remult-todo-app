b7920aa9a98e606418bf420b9fcfa74054ac2777
Cody Price <CodyWilliamPrice@outlook.com>
2024-05-01 10:18:12 -0400

    Set up postgres db

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,9 +1,14 @@
 import { remultExpress } from 'remult/remult-express';
 import { Task } from '../shared/task';
 import { TasksController } from '../shared/TasksController';
+import { createPostgresConnection } from 'remult/postgres';
 
 export const api = remultExpress({
 	entities: [Task],
 	controllers: [TasksController],
 	getUser: (req) => req.session!['user'],
+	dataProvider: createPostgresConnection({
+		connectionString:
+			'postgres://postgres:PostgresEasyPassword@localhost:5432/remulttodo',
+	}),
 });
